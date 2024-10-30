0725df4a5ee0a46e11d280d849a6b95720b34eb3
Hosea <karanja.mungai23@gmail.com>
2024-10-30 19:19:29 +0300

    modifies src/components/EyesOnMe.js

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,7 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    return (
+        <button onBlur={event => console.log('Hey! Eyes on me!')} onFocus={event => console.log('Good!')}>Eyes on me</button>
+    )
+}
+export default EyesOnMe
